fd02670817e40d6f3dd6bdabc8e94e3bb72e36a0
serginhohalves <80188778+serginhohalves@users.noreply.github.com>
2021-08-21 13:53:09 -0300

    Tela de login

---
+++ Cineview/app/src/main/java/com/github/grupo6cineview/cineview/features/login/LoginTelaFragment.kt
@@ -0,0 +1,22 @@
+package com.github.grupo6cineview.cineview.features.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.github.grupo6cineview.cineview.R
+
+
+class LoginTelaFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_tela, container, false)
+    }
+
+
+}
