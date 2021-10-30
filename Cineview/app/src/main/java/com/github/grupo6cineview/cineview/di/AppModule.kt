219638c489445a0d8f9fb9274a86b4615edbdbf3
Pedro Scott <pedroscott.dev@gmail.com>
2021-10-29 23:47:25 -0300

    Fix DI

--- Cineview/app/src/main/java/com/github/grupo6cineview/cineview/di/AppModule.kt
+++ Cineview/app/src/main/java/com/github/grupo6cineview/cineview/di/AppModule.kt
@@ -3,13 +3,14 @@ package com.github.grupo6cineview.cineview.di
 import com.github.grupo6cineview.cineview.features.home.presentation.viewmodel.HomeViewModel
 import com.github.grupo6cineview.cineview.features.movie.presentation.viewmodel.MovieViewModel
 import com.github.grupo6cineview.cineview.features.search.presentation.viewmodel.SearchViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 object AppModule {
 
     val viewModelModules = module {
-        single { HomeViewModel(homeUseCase = get()) }
-        single { SearchViewModel(searchUseCase = get()) }
-        single { MovieViewModel(movieUseCase = get()) }
+        viewModel { HomeViewModel(homeUseCase = get()) }
+        viewModel { SearchViewModel(searchUseCase = get()) }
+        viewModel { MovieViewModel(movieUseCase = get()) }
     }
 }
